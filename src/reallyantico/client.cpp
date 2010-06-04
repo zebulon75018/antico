a87dd1928e99e75469256c9dd3da4f9717e7e2b2
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-06-04 20:58:26 -0300

    Get window type

--- src/reallyantico/client.cpp
+++ src/reallyantico/client.cpp
@@ -1,5 +1,8 @@
 #include "client.hpp"
 #include "frame.h"
+#include "atoms.hpp"
+
+#include <X11/Xatom.h>
 
 Client::Client(Qt::HANDLE window, const QString &type, Dockbar *dockBar, Desk *desk):
     QObject(),
@@ -9,4 +12,45 @@ Client::Client(Qt::HANDLE window, const QString &type, Dockbar *dockBar, Desk *d
     m_desk(desk)
 {
     m_frame = new Frame(window, type, dockBar, desk);
+}
+
+void Client::updateWindowType()
+{
+    Atom typeReturn;
+    int formatReturn;
+    unsigned long count, unused;
+    unsigned char *data = NULL;
+
+    if (XGetWindowProperty(QX11Info::display(), m_window, ATOM(_NET_WM_WINDOW_TYPE),
+			   0l, 2048l, False, XA_ATOM, &typeReturn, &formatReturn,
+			   &count, &unused, &data) == Success)
+    {
+	if (typeReturn == XA_ATOM && formatReturn == 32 && count > 0)
+	{
+	    long *types = (long *)data;
+
+	    for (unsigned long i = 0; i < count; ++i)
+	    {
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_NORMAL))
+		    m_windowType = NormalType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_DESKTOP))
+		    m_windowType = DesktopType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_DOCK))
+		    m_windowType = DockType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_TOOLBAR))
+		    m_windowType = ToolBarType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_MENU))
+		    m_windowType = MenuType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_DIALOG))
+		    m_windowType = DialogType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_UTILITY))
+		    m_windowType = UtilityType;
+		if ((Atom)types[i] == ATOM(_NET_WM_WINDOW_TYPE_SPLASH))
+		    m_windowType = SplashType;
+	    }
+	}
+    }
+
+    if (data)
+	XFree(data);
 }
--- src/reallyantico/client.hpp
+++ src/reallyantico/client.hpp
@@ -16,6 +16,7 @@ public:
     // Supported window types
     enum Type
     {
+	UnknownType,
 	DesktopType,
 	DockType,
 	ToolBarType,
@@ -29,12 +30,19 @@ public:
     // Somewhere in the future will not need the pointer to DockBar and Desk
     Client(Qt::HANDLE window, const QString &type, Dockbar *dockBar, Desk *desk);
 
+    inline Type windowType() const { return m_windowType; }
+
+private:
+    void updateWindowType();
+
 private:
     Qt::HANDLE m_window;
     QString m_type; // TODO: this is ugly!
     Dockbar *m_dockBar;
     Desk *m_desk;
     Frame *m_frame;
+
+    Type m_windowType;
 };
 
 #endif
